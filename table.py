254c443a12a323fc5a9aa19206fe851cbf947f59
Fajrin Ardiansyah <31424025+fajrinard@users.noreply.github.com>
2023-10-18 15:41:08 +0700

    Update table.py

--- table.py
+++ table.py
@@ -34,15 +34,12 @@ def refresh_hk():
     tgl = []
     periode = []
     for x in data.find_all("td", {"class":"text-center"}):
-        numb = []
-        for r in x.find_all("span", {"class":"numball"}):
-            numb.append(r.text)
-        if numb != []:
-            numball.append("".join(numb))
         if "," in x.text:
             tgl.append(x.text)
         if "-" in x.text:
             periode.append(x.text)
+        if "," not in x.text and "-" not in x.text:
+            numball.append(x.text)
     result = {}
     for i,x in enumerate(periode):
         result['data_'+str(i)] = [tgl[i], periode[i], numball[i]]
