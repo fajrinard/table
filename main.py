6d0a3b90480bc0a39e5c56e53bed0f5ef8cf23e7
Fajrin Ardiansyah <31424025+fajrinard@users.noreply.github.com>
2023-10-18 15:29:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.responses import FileResponse
 from table import *
 import uvicorn
 
-app = FastAPI(title='Ardx Lyrics API', version='1.0.0')
+app = FastAPI(title='Ardx Togel API', version='1.0.0')
 
 @app.get('/favicon.ico', include_in_schema=False)
 async def favicon():
@@ -12,14 +12,19 @@ async def favicon():
 @app.get("/")
 def read_root():
     data = {
-        'message': "Welcome to Ardx Lyrics API"
+        'message': "Welcome to Ardx Togel API"
     }
     return data
 
-@app.get("/table/")
+@app.get("/table-sgp/")
 def read_items():
-    data = refresh()
+    data = refresh_sgp()
+    return {'result': data, 'message': 'Table scraped by Ardmosphere. Made with <3'}
+
+@app.get("/table-hk/")
+def read_items():
+    data = refresh_hk()
     return {'result': data, 'message': 'Table scraped by Ardmosphere. Made with <3'}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
