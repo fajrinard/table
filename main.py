1c31d6414e199b4f635c8d516229655286235e35
Fajrin Ardiansyah <31424025+fajrinard@users.noreply.github.com>
2023-11-24 09:02:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse,RedirectResponse
 from table import *
-import uvicorn
+import uvicorn, json
 
-app = FastAPI(title='Ardx Togel API', version='1.0')
+app = FastAPI(title='Ardx Togel API', version='1.0.0')
 
-msg = "Table scraped by Ard. Made with <3"
+msg = "Table scraped by Ard. coded with <3"
 
 @app.get('/favicon.ico', include_in_schema=False)
 async def favicon():
@@ -26,6 +26,21 @@ def read_items():
 @app.get("/table-hk/")
 def read_items():
     data = refresh_result_hk()
+    return {'result': data, 'message': msg}
+
+@app.get("/table-sdy/")
+def read_items():
+    data = refresh_result_sdy()
+    return {'result': data, 'message': msg}
+
+@app.get("/table-tw/")
+def read_items():
+    data = refresh_result_tw()
+    return {'result': data, 'message': msg}
+
+@app.get("/table-cam/")
+def read_items():
+    data = refresh_result_cam()
     return {'result': data, 'message': msg}
 
 @app.get("/table-macau/")
@@ -63,4 +78,4 @@ async def custom_404_handler(_, __):
     return RedirectResponse("/")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0")
